403e174dce09389759590a4f9463b6c6ea373fcc
chien210030 <chien210030@gmail.com>
2024-05-22 23:31:19 +0700

    bai moi

--- Object/Object.js
+++ Object/Object.js
@@ -34,9 +34,39 @@ parrot.age = 18;
 console.log(parrot.getName());
 console.log(parrot)
 
+//  OBJECT  prototype
+// OBJECT prototype sử dụng khi mà một OBject constructor đã được khởi tạo
+
+function Student(firstName, lastName){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    }
+    
+    Student.prototype.getFullName = function(){
+        return `${this.firstName} ${this.lastName}`
+    }
+    
+    // Ví dụ khi sử dụng
+    var student = new Student('Long', 'Bui');
+    
+    console.log(student.firstName);  // 'Long'
+    console.log(student.lastName);  // 'Bui'
+    console.log(student.getFullName());  // 'Long Bui'
 
 
 
 
+// DATE  Đối tượng Date 
+function getNextYear(){
+    var date = new Date();
+    var nextyear = date.getFullYear()+1;
+    var month = date.getMonth();
+    var day = date.getDate();
+    console.log("tháng:",month)
+    console.log("nam:",nextyear)
+        return nextyear;
+    }
+getNextYear();
+// MATH OBJECT
 
 
